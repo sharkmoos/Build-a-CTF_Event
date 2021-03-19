74db73628ab8a73282eacdc81b36bb77d49b6092
sharkmoos <muddy117@gmail.com>
2021-03-19 14:41:25 +0000

    Added basic command injection

--- docker/flask/create_db.py
+++ docker/flask/create_db.py
@@ -16,15 +16,10 @@ if __name__ == "__main__":
     admin_uuid=str(uuid.uuid4())
     noah_uuid=str(uuid.uuid4())
     users = [
-        User(username='admin', email='admin@sharklabs.local', uuid=admin_uuid, admin_cap=True, password="0d3ae6d144edfb313a9f0d32186d4836791cbfd5603b2d50cf0d9c948e50ce68"),] # JWTW1ns
+        User(username='admin', email='admin@sharklabs.local', uuid=admin_uuid, admin_cap=True, password="0d3ae6d144edfb313a9f0d32186d4836791cbfd5603b2d50cf0d9c948e50ce68"),
+        User(username="intern", email="intern@canely.com", uuid=admin_uuid, admin_cap=True, password="fb001dfcffd1c899f3297871406242f097aecf1a5342ccf3ebcd116146188e4b")
+        ] # JWTW1ns
     print("@>Adding Users")
     addUsers(users)
-    print("@>Creating Notes")
-    notes = [
-        Notes(owner_uuid=admin_uuid, title="Need to fix config", note="Have to fix this issue where PHP files are being executed :/. This can be a potential security issue for the server. (Update. Fixed"),
-        Notes(owner_uuid=admin_uuid, title="Backups are scheduled", note="Finally! Regular backups are necessary. Thank god it's all easy on server."),
-        Notes(owner_uuid=admin_uuid, title="SSH Key", note="I stored my ssh key here, for when I need access to the server remotely this webapp is secure enough. \n ")
-    ]
-    print("@>Adding Notes")
-    addNotes(notes)
     print("@>DONE. Database initialized and populated.")
+pwd
