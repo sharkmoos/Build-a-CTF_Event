8175a2aaf844189e042c6ecf70807bf6abe536fb
sharkmoos <muddy117@gmail.com>
2021-03-24 16:55:16 +0000

    Update runapp.sh

--- docker/flask/runapp.sh
+++ docker/flask/runapp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 gcc /home/manager/source.c -o runme 
 chown manager /home/manager/runmme 
+sudo -u manager chmod +s /home/manager/runmme 
 sudo -u intern python3 /home/intern/create_db.py
 sudo -u intern python3 /home/intern/main.py
